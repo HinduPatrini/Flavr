5f44cfa1e2e06d07baa92f7b108ea1048b2bac84
HinduPatrini <hindupatrini16@gmail.com>
2026-06-01 13:51:21 +0530

    updated aicontrollers

--- server/controllers/aiController.js
+++ server/controllers/aiController.js
@@ -25,7 +25,7 @@ const generateRecipe = async (req, res) => {
     `;
 
     const response = await groq.chat.completions.create({
-      model: "llama3-8b-8192",
+      model: "llama-3.3-70b-versatile",
       messages: [{ role: "user", content: prompt }],
       temperature: 0.7,
     });
