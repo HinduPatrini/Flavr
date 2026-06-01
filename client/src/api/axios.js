166ee414554489c5893ce8d18dc680d72b9a1ed2
HinduPatrini <hindupatrini16@gmail.com>
2026-06-01 14:26:45 +0530

    updated axios.js

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -1,26 +1,16 @@
-import axios from "axios";
+import axios from "axios"
 
-const API = axios.create({
-  baseURL: import.meta.env.VITE_API_URL
-    ? `${import.meta.env.VITE_API_URL}/api`
-    : "http://localhost:5000/api",
-  headers: {
-    "Content-Type": "application/json",
-  },
-});
+const instance = axios.create({
+  baseURL: import.meta.env.VITE_API_URL,
+  withCredentials: true,
+})
 
-// Interceptor to add auth token to requests
-API.interceptors.request.use(
-  (config) => {
-    const token = localStorage.getItem("token");
-    if (token) {
-      config.headers.Authorization = `Bearer ${token}`;
-    }
-    return config;
-  },
-  (error) => {
-    return Promise.reject(error);
+instance.interceptors.request.use((config) => {
+  const token = localStorage.getItem("token")
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`
   }
-);
+  return config
+})
 
-export default API;
+export default instance
